1dcfee9a1a66a47b68e83f8c2eea902e4c121dc6
Darien Sessions <darien.sessions@gmail.com>
2024-09-05 07:34:03 -0400

    removing console functions

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const router = require("./routes");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch(console.error);
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
 
@@ -25,7 +20,4 @@ app.use((req, res, next) => {
 
 app.use("/", router);
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+app.listen(PORT);
