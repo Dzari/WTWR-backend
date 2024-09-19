7bf8d0283db27ed01d9ed9e85fdd1706fea2ba48
Darien Sessions <darien.sessions@gmail.com>
2024-09-19 08:00:35 -0400

    update login api

--- controllers/users.js
+++ controllers/users.js
@@ -86,7 +86,7 @@ const login = (req, res) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: "7d",
       });
-      res.send({ token });
+      res.send({ token, user });
     })
     .catch((err) => {
       if (err.message === "Incorrect email or password") {
