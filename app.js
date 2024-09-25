b093f231ba2cb7518d91f387ba1994c46c118c71
Darien Sessions <darien.sessions@gmail.com>
2024-09-25 10:58:45 -0400

    adding logs with winston

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const router = require("./routes");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { errorHandling } = require("./middlewares/errorHandling");
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
@@ -17,7 +18,11 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use("/", router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
