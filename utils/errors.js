b40768c56c9b763b6bc11ffac56a25bc067e64d5
Darien Sessions <darien.sessions@gmail.com>
2024-09-25 10:14:30 -0400

    deleting unused variables

--- utils/errors.js
+++ utils/errors.js
@@ -1,11 +1,5 @@
-const BAD_REQUEST = 400;
-const UNAUTHORIZED = 401;
-const FORBIDDEN = 403;
-const NOT_FOUND = 404;
-const HTTPERROR = 409;
 const DEFAULT = 500;
 const SUCCESS = 200;
-const forbiddenMessage = "You are not authorized to perform this action";
 
 class BadRequestError extends Error {
   constructor(message) {
@@ -48,7 +42,5 @@ module.exports = {
   BadRequestError,
   ForbiddenError,
   ConflictError,
-  DEFAULT,
   SUCCESS,
-  forbiddenMessage,
 };
